a9f9105582b51c7c3462ae5a719eda0d797e15c8
Luke Eltiste <lukeeltiste@csu.fullerton.edu>
2021-02-15 13:08:52 -0800

    more test categories

--- GradeManager/main.cpp
+++ GradeManager/main.cpp
@@ -5,9 +5,12 @@ int main()
 {
   Course compSci( "Computer Science" );
 
-  compSci.addCategory( "Homework", 20.0 );
-  compSci.addCategory( "Project", 30.0 );
-  compSci.addCategory( "Quiz", 40.0 );
+  compSci.addCategory( "Attendance", 10.0 );
+  compSci.addCategory( "Participation", 5.0 );
+  compSci.addCategory( "Knowledge Checks", 15.0 );
+  compSci.addCategory( "Projects", 30.0 );
+  compSci.addCategory( "Midterm", 20.0 );
+  compSci.addCategory( "Final", 20.0 );
 
   Assignment homework( "Homework 1", 100.0, 100.0, "Homework" );
   Assignment project( "Project 1", 100.0, 100.0, "Project" );
